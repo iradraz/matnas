c6adf278b9f8f1b6f2d1ccd4c40d347775569640
Irad <Irad@irad-laptop>
2018-05-13 21:18:52 +0300

    first commit

---
+++ application/modules/firsttry/controllers/firsttry.php
@@ -0,0 +1,11 @@
+    <?php
+	class Firsttry extends MX_Controller 
+	{
+		function __construct()
+		{
+			parent::__construct();
+		}
+                function hello(){
+                    echo "working abc";
+                }
+	}
